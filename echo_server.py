e52383e28297d595d6ded267dfeccb832646763c
Jagoda <jagoda@localhost.localdomain>
2019-03-03 17:20:27 +0100

    settimeout

--- echo_server.py
+++ echo_server.py
@@ -1,12 +1,12 @@
 import socketserver
 import socket
 
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_addres = ('127.0.0.1',1024)
 
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
 server.bind(server_addres)
+server.settimeout(5)
 server.listen(5)
 
 print ("Serv addr:", server_addres)
@@ -19,6 +19,7 @@ connection, addr = server.accept()
 
 while True:
     data = connection.recv(1024)
+    print (data.decode())
     if not data:
         break
     connection.sendall(data)
