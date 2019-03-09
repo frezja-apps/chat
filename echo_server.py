c7e9c6830fa3f3e266cfc9b3fd46ab9244f0561c
Jagoda <jagoda@localhost.localdomain>
2019-03-09 18:14:25 +0100

    multiple connections

--- echo_server.py
+++ echo_server.py
@@ -1,46 +1,69 @@
 import socketserver
-import socket
-import threading
-import _thread
+import socket, select, queue
 
 server_addres = ('127.0.0.1',1024)
-
-thr_lock = threading.Lock()
-
-def threads(co):
-    while True:
-        data = connection.recv(1024)
-        print ("Message:",data.decode())
-        if not data:
-            thr_lock.release()
-            break
-
-        connection.sendall(data)
-    connection.close()
-    print ("Connection close")
-
-print ("Serv addr:", server_address)
-
-#inputs = [server]
-#outputs = []
-#messege_queues = {}
+print ("Serv addr:", server_addres)
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
 server.bind(server_addres)
 server.listen(5)
+server.setblocking(0)
 
-while True:
-    connection, addr = server.accept()
-    thr_lock.acquire()
-    print("Connected to:",addr)
-    start_new_thread(threads,(co,))
-    #while True:
-    #    data = connection.recv(1024)
-    #    print ("Message:",data.decode())
-    #    if not data:
-    #        thr_lock.release()
-    #        break
-    #    connection.sendall(data)
-connection.close()
-print ("Connection close")
+inputs = [server]
+outputs = []
+message_queues = {}
+
+while inputs:
+    rlist, wlist, elist = select.select(inputs, outputs, inputs)
+    for c in rlist:
+        if c is server:
+            connection, addr = c.accept()
+            print ("Connected to", addr)
+            connection.setblocking(0)
+            inputs.append(connection)
+            message_queues[connection] = queue.Queue()
+            #print ("Inp:", inputs)
+        else:
+            data = c.recv(1024)
+            print ("Message: ", data.decode())
+            #c.send(data)
+            if data:
+                message_queues[c].put(data)
+                if c not in outputs:
+                    outputs.append(c)
+                    #print ("Out:", outputs)
+            else:
+                if c in outputs:
+                    outputs.remove(c)
+                inputs.remove(c)
+                c.close()
+                print ("Disconnect")
+                del message_queues[c]
+
+    for c in wlist:
+        try:
+            next_msg = message_queues[c].get_nowait()
+        except queue.Empty:
+            outputs.remove(c)
+        else:
+            c.send(next_msg)
+
+    for c in elist:
+        inputs.remove(c)
+        if c in outputs:
+            outputs.remove(c)
+        c.close()
+        del message_queues[c]
+    
+
+#connection, addr = server.accept()
+#print("Connected to:",addr)
+
+#    while True:
+#        data = connection.recv(1024)
+#        print ("Message:",data.decode())
+#        if not data:
+#            break
+#       connection.sendall(data)
+#server.close()
+#print ("Connection close")
